593f9230cffce3d00f534e157c56588a56019c9b
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-04-28 15:35:41 +0200

    feat: create MapContainer component with Leaflet logic (#5)

---
+++ monparcours/src/components/MapContainer.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div id="map" style="height: 100vh; width: 100%;"  ></div>
+</template>
+
+<script setup>
+
+import { onMounted } from 'vue';
+import L from 'leaflet';
+
+onMounted(()=>{
+
+    // When the app opens, the map of Lausanne will appear
+    const map=L.map('map').setView([46.7826,6.6449],13) // yverdon
+
+    // z: zoom  x and y : Coordinate squares
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        attribution:'© OpenStreetMap contributors'
+    }).addTo(map);
+})
+
+</script>
+
+
