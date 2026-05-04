48871e0979b98a69930a0b9f420f85905efa35da
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-05-04 14:08:02 +0200

    add : setup basic Sidebar structure to hold the steps list (#9)

---
+++ monparcours/src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="sidebar">
+        <h3>My etapes</h3>
+        <div v-if="etapes.length === 0" class="empty-msg">
+            No points added. Click on the map!
+        </div>
+
+        <EtapeItem
+        v-for="(Point,index) in etapes"
+        :key="point.id"
+        :etape="point"
+        :index="index+1"
+
+
+        />
+
+
+
+    </div>
+
+    </template>
+
+
+<script setup>
+
+import { point } from 'leaflet';
+import EtapeItem from './EtapeItem.vue';
+defineProps(['etapes'])
+
+</script>
+
+<style scoped>
+.sidebar {
+    width: 300px;
+    background: #f4f4f4;
+    border-left: 1px solid #ddd;
+    padding: 15px;
+    overflow-y:auto ;
+
+}
+
+.empty-msg {
+    color: #888;
+    font-style: italic;
+    margin-top: 20px;
+}
+
+</style>
