e09971f1d1c85eb51236633df497e253f93f9141
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-05-04 14:00:37 +0200

    add : EtapeItem component to display step details in sidebar(#20)

---
+++ monparcours/src/components/EtapeItem.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="etape-card">
+        <strong> #{{ index }}</strong> - {{ etape.name }}
+    </div>
+
+    </template>
+
+    <script setup>
+
+    defineProps(['etape','index']);
+
+</script>
+
+<style scoped>
+
+.etape-card {
+    background: white;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+    border-left: 5px solid #3388ff;
+}
+
+</style>
