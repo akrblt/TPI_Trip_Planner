66cc3f3ad824a9ddc4271c6e083d38489d7b1ce2
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-05-07 11:18:20 +0200

    feat: create  RouteStorage component for saving route and marker data (#14)

---
+++ monparcours/src/components/RouteStorage.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="storage-panel">
+    <button @click="saveRoute">Save</button>
+    <button @click="loadRoute">Load</button>
+</div>
+
+
+</template>
+
+<script setup> 
+const props=defineProps({
+    etapes:Array
+});
+
+const emit=defineEmits([
+    "load-route"
+]);
+
+const saveRoute=()=>{
+    localStorage.setItem("myRoute",JSON.stringify(props.etapes));
+    alert("Saved!");
+};
+
+const loadRoute=()=>{
+    const data=localStorage.getItem("myRoute");
+    if(!data) return alert("No route");
+    emit("load-route",JSON.parse(data))
+}
+
+</script>
