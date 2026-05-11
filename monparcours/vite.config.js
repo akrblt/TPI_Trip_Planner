4188fc993b401a23d201e383428c5c3f2844df4b
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-05-11 09:19:59 +0200

    setup: Vitest and testing environment configuration (#24)

--- monparcours/vite.config.js
+++ monparcours/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    // il must be jsdom for vitest
+    environment: 'jsdom',
+    globals:true, 
+  }
 })
